1e6df88c74b8c8176e70a2efc7fa8ac987c5a04e
HelmyHassan <71571217+HelmyHassan@users.noreply.github.com>
2021-10-09 16:53:27 +0200

    update main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function( $ ) {
 
-  // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
@@ -12,8 +11,7 @@ jQuery(document).ready(function( $ ) {
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
     return false;
   });
-
-  // Header fixed on scroll
+  
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
@@ -34,7 +32,6 @@ jQuery(document).ready(function( $ ) {
   // Initiate the wowjs animation library
   new WOW().init();
 
-  // Initialize Venobox
   $('.venobox').venobox({
     bgcolor: '',
     overlayColor: 'rgba(6, 12, 34, 0.85)',
@@ -42,7 +39,6 @@ jQuery(document).ready(function( $ ) {
     closeColor: '#fff'
   });
 
-  // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {
       opacity: 'show'
@@ -50,7 +46,6 @@ jQuery(document).ready(function( $ ) {
     speed: 400
   });
 
-  // Mobile Navigation
   if ($('#nav-menu-container').length) {
     var $mobile_nav = $('#nav-menu-container').clone().prop({
       id: 'mobile-nav'
@@ -90,7 +85,6 @@ jQuery(document).ready(function( $ ) {
     $("#mobile-nav, #mobile-nav-toggle").hide();
   }
 
-  // Smooth scroll for the menu and links with .scrollto classes
   $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -124,7 +118,6 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Gallery carousel (uses the Owl Carousel library)
   $(".gallery-carousel").owlCarousel({
     autoplay: true,
     dots: true,
@@ -134,14 +127,11 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Buy tickets select the ticket type on click
   $('#buy-ticket-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var ticketType = button.data('ticket-type');
     var modal = $(this);
     modal.find('#ticket-type').val(ticketType);
   })
-
-// custom code
 
 });
